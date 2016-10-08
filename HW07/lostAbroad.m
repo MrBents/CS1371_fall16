4a13d9fdc9680c99881b78f95fdae733981d4edb
Max Bentata <mbentata@gatech.com>
2016-10-08 13:32:56 -0400

    Added HW07

--- HW07/lostAbroad.m
+++ HW07/lostAbroad.m
@@ -1,36 +1,40 @@
 function out = lostAbroad( map, directions )
     sentence = [];
 
+%   for every coordinate in directions
     for i = 1:length(directions)-1
         lc = i;
+%       get start coordinates
         spx = directions(1, lc);
         spy = directions(2, lc);
-
+        
+%       add letter at that location
         sentence = [sentence map(spx, spy)];
-
+%       get next coordinate
         next_coordx = directions(1, lc + 1);
         next_coordy = directions(2, lc + 1);
-
+%       compute distance difference between start and future coords
         deltax = next_coordx - spx;
         deltay = next_coordy - spy;
         
+%       compute on which axis is the difference  
         if deltax == 0 && deltay == 0
-            map(spx, spy)
             sentence = [sentence map(spx, spy)];
         elseif deltax == 0
             maxit = deltay;
         elseif deltay == 0
             maxit = deltax;
         end
-
+        
         negative = maxit < 0;
         if abs(maxit) ~= 1
+%           for every unit of difference loop once
             for j = 1:abs(maxit)-1
 
                 if negative
                    j = -j; 
                 end
-
+%               add letters on the way to coords
                 if deltay == 0
                     l = map(spx + j, spy);
                     sentence = [sentence map(spx + j, spy)];
@@ -41,8 +45,10 @@ function out = lostAbroad( map, directions )
             end
         end
     end
+%   add last coord (which is excluded in the looping)
     sentence = [sentence map(directions(1, end), directions(2, end))];
 
+%   create sentence according to specs
     if sum(sentence == '#') > 0
         out = 'No record of this person exists. -Government';
         return
--- HW07/moveSnake.m
+++ HW07/moveSnake.m
@@ -1,12 +1,16 @@
 function [ out ] = moveSnake(board, moves)
     length(moves)
+%   loop once for every move
     for i = 1:length(moves)
         verdict = checkCollision(board, moves(i));
+%       if we crash onto a wall or ourselves end the game
         if verdict == false
              out = 'Game Over!';
              return;
+%       if all clear then move
         elseif verdict == true
             board = makeMoves(board, moves(i), 'move');
+%       if there's a cookie, grow
         elseif strcmp(verdict,'lollipop')
             board = makeMoves(board, moves(i), 'grow');
             board = spawnCookie(board);
@@ -17,11 +21,12 @@ function [ out ] = moveSnake(board, moves)
 end
 
 function new_map = makeMoves(bd, move, type)
-    [maxvar, indx] = max(max(bd));
-    snake = [1:maxvar];
+
+%   getting head of snake
+    maxvar = max(max(bd));
     
     [rows, cols] = find(bd);
-    
+%   getting coords of head
     [row_top, col_top] = find(bd == maxvar);
     
     
@@ -39,15 +44,20 @@ function new_map = makeMoves(bd, move, type)
     
     if strcmp(type,'move')
         for j = 1:length(rows)
+%           get current coords
             [rowz, colz] = find(bd == maxvar - (j - 1));
+%           turn space behind 1 into 0
             if maxvar - (j - 1) == 1
                 bd(rowz, colz) = 0;
             end
+%           exchange values from old to new coords
             bd(new_coords(1), new_coords(2)) = maxvar - (j - 1);
+%           get new coords
             new_coords = [rowz, colz];
         end
         new_map = bd;
     elseif strcmp(type,'grow')
+%       assign the cookie location a value of maxvar + 1
         bd(new_coords(1), new_coords(2)) = maxvar + 1;
         new_map = bd;
     end
